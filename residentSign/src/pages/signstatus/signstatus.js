9c8770a58199cf0f0aabf6d053a9a18d7e9d1f55
zhangbinglin <zhangbinglin>
2017-08-23 13:46:36 +0800

    first commit

---
+++ residentSign/src/pages/signstatus/signstatus.js
@@ -0,0 +1,77 @@
+// 获取全局应用程序实例对象
+// const app = getApp()
+let util = require('../../utils/util.js')
+Page({
+  data: {
+    queryFamilyArr: [],
+    queryFamilyArrStatus1: [],
+    queryFamilyArrStatus24: [],
+    imgReqUrl: 'http://122.224.131.235:9088/hcn-web/upload/'
+  },
+  waitForSign(e) {
+    wx.setStorageSync('dosignPersonInfo', JSON.stringify(e.currentTarget.dataset.item))
+    wx.navigateTo({
+      url: '../dosign/dosign'
+    })
+  },
+  onLoad() {
+    let params = ["hcn.shenzhen", '9fa92b25-42f4-4895-b253-d5424fc7d72d']
+    util.commonAjaxKy(JSON.stringify(params), 'pcn.residentSignService', 'queryFamily')
+      .then(res => {
+        if (res.code === 200) {
+          console.log(res.body)
+          // this.data.queryFamilyArr = res.body
+          this.setData({
+            queryFamilyArr: res.body
+          })
+          let that = this, tempArr24 = [], tempArr1 = []
+          this.data.queryFamilyArr.forEach(item => {
+            if (item.status === '1' || item.status === '2') { //2为签约完成，1为待签约
+              tempArr24.push(item)
+            } else {
+              tempArr1.push(item)
+            }
+          })
+          that.setData({
+            queryFamilyArrStatus24: tempArr24
+          })
+          that.setData({
+            queryFamilyArrStatus1: tempArr1
+          })
+        }
+      })
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+    // TODO: onReady
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {},
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+    // TODO: onHide
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+    // TODO: onUnload
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+    // TODO: onPullDownRefresh
+  }
+})
---
+++ residentSign/src/utils/util.js
@@ -0,0 +1,45 @@
+let base = 'http://122.224.131.235:9090'
+
+function commonAjaxKy(params, ServiceId, ServiceMethod) {
+  return new Promise((resolve, reject) => {
+    wx.request({
+      url: `${base}/pcn-core/dataProxyController/hcnJsonRequestProxy?short_url=*.jsonRequest&params=${params}`,
+      data: {},
+      header: {
+        "X-Access-Token": '55cf67c7-0933-4ed3-b5f6-6fb6e52467bc',
+        "X-Service-Id": ServiceId,
+        "X-Service-Method": ServiceMethod,
+        "Content-Type": "application/json"
+      },
+      success: function(res, error) {
+        res.data ? resolve(res.data) : reject(error)
+      }
+    })
+  })
+  // return Promise.all([promise]).then(res => {
+  //   console.log(res);
+  // })
+}
+
+
+// function formatTime(date) {
+//   var year = date.getFullYear()
+//   var month = date.getMonth() + 1
+//   var day = date.getDate()
+//
+//   var hour = date.getHours()
+//   var minute = date.getMinutes()
+//   var second = date.getSeconds()
+//
+//
+//   return [year, month, day].map(formatNumber).join('/') + ' ' + [hour, minute, second].map(formatNumber).join(':')
+// }
+//
+// function formatNumber(n) {
+//   n = n.toString()
+//   return n[1] ? n : '0' + n
+// }
+
+module.exports = {
+  commonAjaxKy: commonAjaxKy
+}
